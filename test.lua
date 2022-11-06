d5469b711d6e3930ec9696d8b9cb5b4ff5586f47
Ted Osborne <eccentricon@users.noreply.github.com>
2022-11-06 12:05:26 -0500

    first commit

---
+++ test.lua
@@ -0,0 +1,10 @@
+#!/usr/bin/env lua
+-- Programming in Lua, 4th ed.
+
+print("Hello, world!")
+
+print("arg0: ", arg[0], type(arg[0]))
+print("arg1: ", arg[1], type(arg[1]))
+print("arg2: ", arg[2], type(arg[2]))
+print("arg3: ", arg[3], type(arg[3]))
+print("arg4: ", arg[4], type(arg[4]))
